e6d63acac1b5b8bf58a8e0e885c1a13008852fda
Unknown <zdt1997@gmail.com>
2019-02-01 20:26:05 -0600

    Refactor for dynamic launch files

--- scripts/launch_nav.py
+++ scripts/launch_nav.py
@@ -7,51 +7,43 @@ from std_msgs.msg import String
 def navigation_node():
     rospy.init_node('navigation')
 
-    PARAM_PREFIX = '/navigation/'
+    # NAV_NAMESPACE = '/navigation'
+    ROBOT_NAMESPACE = '/robots'
     PACKAGE_NAME = 'multi_robot_path_planner'
     cli_args = []
 
     # Get params from parameter server
-    rviz = rospy.get_param(PARAM_PREFIX + 'rviz')
-    environment = rospy.get_param(PARAM_PREFIX + 'environment')
-    mapping = rospy.get_param(PARAM_PREFIX + 'mapping')
-    robots = rospy.get_param(PARAM_PREFIX + 'robots')
+    # navigation = rospy.get_param(NAV_NAMESPACE)
+    robots = rospy.get_param(ROBOT_NAMESPACE)
 
-    # Set up rviz launch args
-    if (rviz == True):
-        cli_args.append([PACKAGE_NAME, 'rviz.launch', 'single:=true' if len(
-            robots) == 1 else 'single:=false'])
-
-    # Set up environment launch args
-    cli_args.append([environment['package'], environment['launch_file']])
-#    for arg, value in environment['args'].iteritems():
-#        cli_args[-1].append(arg + ':=' + value)
+    # Extract navigation params
+    # mapping = navigation.get('mapping', {})
 
     # Set up mapping launch args for either map_server or gmapping (for now)
-    if mapping['node_package'] == 'map_server':
-        cli_args.append([PACKAGE_NAME, 'map_server.launch',
-                         'map_package:=' + mapping['map_package'],
-                         'map_path:=' + mapping['map_path'],
-                         'map_frame:=' + mapping['map_frame'],
-                         'map_topic:=' + mapping['map_topic']])
+    # for mapping_node in mapping:
+    #     if mapping_node['node_package'] == 'map_server':
+    #         cli_args.append([PACKAGE_NAME, 'map_server.launch',
+    #                          'map_package:=' + mapping_node['map_package'],
+    #                          'map_path:=' + mapping_node['map_path'],
+    #                          'map_frame:=' + mapping_node['map_frame'],
+    #                          'map_topic:=' + mapping_node['map_topic'],
+    #                          'node_name:=' + mapping_node['node_name']])
 
-    elif mapping['node_package'] == 'gmapping':
-        cli_args.append([PACKAGE_NAME, 'gmapping.launch',
-                         'map_frame:=' + mapping['map_frame'],
-                         'map_topic:=' + mapping['map_topic'],
-                         'odom_frame:=' + mapping['odom_frame'],
-                         'scan_topic:=' + mapping['scan_topic']])
+    #     elif mapping_node['node_package'] == 'gmapping':
+    #         cli_args.append([PACKAGE_NAME, 'gmapping.launch',
+    #                          'map_frame:=' + mapping_node['map_frame'],
+    #                          'map_topic:=' + mapping_node['map_topic'],
+    #                          'odom_frame:=' + mapping_node['odom_frame'],
+    #                          'scan_topic:=' + mapping_node['scan_topic'],
+    #                          'node_name:=' + mapping_node['node_name']])
 
     # Set up robot launch args
     for robot in robots:
         cli_args.append([PACKAGE_NAME, 'single_robot.launch'])
-        cli_args[-1].append('map_topic:=' + mapping['map_topic'])
         tf_prefix = robots[robot]['tf_prefix']
 
         for arg, value in robots[robot].iteritems():
-            if arg == 'tf_prefix':
-                continue
-            elif arg == 'move_base':
+            if arg == 'move_base':
                 cli_args[-1].append('move_base_config_package:=' +
                                     value['config_package'])
                 cli_args[-1].append('move_base_config_path:=' +
@@ -73,9 +65,11 @@ def navigation_node():
                 if value.startswith('/'):
                     cli_args[-1].append(arg + ':=' + value)
                 elif tf_prefix == '' or tf_prefix.endswith('/'):
-                    cli_args[-1].append(arg + ':=' + tf_prefix + value)
+                    cli_args[-1].append(arg + ':=' +
+                                        tf_prefix + value)
                 else:
-                    cli_args[-1].append(arg + ':=' + tf_prefix + '/' + value)
+                    cli_args[-1].append(arg + ':=' +
+                                        tf_prefix + '/' + value)
 
             else:
                 cli_args[-1].append(arg + ':=' + value)
